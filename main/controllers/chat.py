39098ce8a9b12f39238dd43c4be8be3f70905a1b
Hiep Nguyen <hoanghiepnguyen106@gmail.com>
2021-10-26 07:32:37 +0700

    feat: add user model to message model and validate message content

--- main/controllers/chat.py
+++ main/controllers/chat.py
@@ -1,9 +1,12 @@
 from datetime import datetime
 
+from pydantic import ValidationError
 from starlette.websockets import WebSocket, WebSocketDisconnect
 
 from main import app, chat_room, logger
+from main.database import serialize_doc
 from main.schemas.message import MessageModel
+from main.schemas.user import UserModel
 from main.services import message as message_service
 from main.services import user as user_service
 
@@ -15,22 +18,29 @@ async def websocket_endpoint(websocket: WebSocket, user_id: str):
         logger.info(f"User {user_id} not found")
         await websocket.close()
         return
+    user = UserModel(**serialize_doc(user_doc))
 
     await chat_room.connect(websocket)
     try:
         while True:
             content = await websocket.receive_text()
-            message = _construct_message(user_id, content)
-            await chat_room.broadcast(message)
-            await message_service.create_message(message)
+            try:
+                message = _construct_message(user, content)
+                await chat_room.broadcast(message)
+                await message_service.create_message(message)
+            except ValidationError as e:
+                await websocket.send({
+                    "error": True,
+                    "detail": e
+                })
     except WebSocketDisconnect:
         logger.info(f"User {user_id} disconnected")
         chat_room.disconnect(websocket)
 
 
-def _construct_message(user_id: str, content: str) -> MessageModel:
+def _construct_message(user: UserModel, content: str) -> MessageModel:
     return MessageModel(
-        user_id=user_id,
+        user=user,
         content=content,
         created_at=datetime.utcnow().isoformat()
     )
--- main/schemas/message.py
+++ main/schemas/message.py
@@ -1,15 +1,16 @@
 from datetime import datetime
 from typing import Optional, List
 
-from pydantic import BaseModel
+from pydantic import BaseModel, constr
 
 from main.schemas.pagination import PaginationModel
+from main.schemas.user import UserModel
 
 
 class MessageModel(BaseModel):
     id: Optional[str]
-    user_id: str
-    content: str
+    user: UserModel
+    content: constr(strip_whitespace=True, min_length=1, max_length=1000)
     created_at: datetime
 
 
