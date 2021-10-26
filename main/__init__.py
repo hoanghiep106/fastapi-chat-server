f916146b7497bc673ac3fb7e06e0b24c01d20082
Hiep Nguyen <hoanghiepnguyen106@gmail.com>
2021-10-26 07:31:03 +0700

    docs: use redoc for API docs

--- main/__init__.py
+++ main/__init__.py
@@ -1,6 +1,7 @@
 import logging
 
 from fastapi import FastAPI
+from fastapi.openapi.docs import get_redoc_html
 
 from main.config import Config
 from main.services.chat import ChatRoom
@@ -9,12 +10,16 @@ logger = logging.getLogger("uvicorn.error")
 config = Config()
 
 chat_room = ChatRoom()
-app = FastAPI()
+app = FastAPI(docs_url=None, redoc_url=None)
 
 
 # Avoiding circular imports
 def register_controllers():
     import main.controllers
+
+    @app.get("/redoc")
+    async def get_documentation():
+        return get_redoc_html(openapi_url="openapi.json", title="docs")
 
 
 register_controllers()
